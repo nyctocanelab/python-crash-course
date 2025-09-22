044fedfdb6130e163a6b9a8e7dff54e34cbab3c1
rahulsdubey02 <rahulsdubey02@gmail.com>
2025-09-23 05:21:03 +0530

    Hello World in Python

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+import this
+print("Hello World!!")
+message = "python crash course!"
+# message = message.removeprefix("python ")
+print(message.title())
+print(message.upper())
+print(message.lower())
