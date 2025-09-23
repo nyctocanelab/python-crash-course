3347b5644bb0b56a00b12f6788454443debb0cb9
rahulsdubey02 <rahulsdubey02@gmail.com>
2025-09-24 01:44:30 +0530

    short excercises on list

---
+++ short_exercises/dream_car_list_33.py
@@ -0,0 +1,5 @@
+favorite_car = ["audi", "bmw", "subaru", "toyota"]
+print(f"I would like to own a {favorite_car[0].title()} car.")
+print(f"I would like to own a {favorite_car[1].title()} car.")
+print(f"I would like to own a {favorite_car[2].title()} car.")
+print(f"I would like to own a {favorite_car[3].title()} car.")
---
+++ short_exercises/greetings_32.py
@@ -0,0 +1,6 @@
+names = ["alice", "bob", "charlie", "david", "eve"]
+print(f"Hello, Nice meeting you, {names[0]}.")
+print(f"Hello, Nice meeting you, {names[1]}.")
+print(f"Hello, Nice meeting you, {names[2]}.")
+print(f"Hello, Nice meeting you, {names[3]}.")
+print(f"Hello, Nice meeting you, {names[4]}.")
